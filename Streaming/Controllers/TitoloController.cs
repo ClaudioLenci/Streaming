59327a4cbc623e072d1c431d9593556d83ed0dfd
LenciCla <lenciclaudio06@gmail.com>
2024-11-18 09:22:18 +0100

    Create controllers

---
+++ Streaming/Controllers/TitoloController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Streaming.Controllers
+{
+    public class TitoloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Streaming/Controllers/UtenteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Streaming.Controllers
+{
+    public class UtenteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
