584199e4ddde0c6abef4f0b3515df1302e92c2f2
LenciCla <lenciclaudio06@gmail.com>
2024-11-21 12:26:01 +0100

    Creazione DatabaseService e installazione pacchetti

---
+++ Streaming/Services/DatabaseService.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.SqlClient;
+
+namespace Streaming.Services
+{
+    public class DatabaseService
+    {
+        SqlConnection conn;
+
+        DatabaseService(string databaseUrl)
+        {
+            conn = new SqlConnection(databaseUrl);
+        }
+    }
+}
