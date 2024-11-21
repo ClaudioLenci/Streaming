6c199c0dc1b820ac1aea3c8ce5e38a4aa506ce95
LenciCla <lenciclaudio06@gmail.com>
2024-11-21 12:47:19 +0100

    Inject connection in every controller

--- Streaming/Controllers/ContenutoController.cs
+++ Streaming/Controllers/ContenutoController.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
 
 namespace Streaming.Controllers
 {
     public class ContenutoController : Controller
     {
+        private readonly SqlConnection db;
+        public ContenutoController(SqlConnection _db)
+        {
+            db = _db;
+        }
+
         public IActionResult Index()
         {
             return View();
--- Streaming/Controllers/UtenteController.cs
+++ Streaming/Controllers/UtenteController.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
 
 namespace Streaming.Controllers
 {
     public class UtenteController : Controller
     {
+        private readonly SqlConnection db;
+
+        public UtenteController(SqlConnection _db)
+        {
+            db = _db;
+        }
+
         public IActionResult Index()
         {
             return View();
